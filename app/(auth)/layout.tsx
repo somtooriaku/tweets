dfa959da6fd22dc7af05aedc0a1e95cba30b9081
somtooriaku <59647845+somtooriaku@users.noreply.github.com>
2023-08-15 05:37:00 -0400

    #Authorization files refused to push??

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+export const metadata = {
+    title: "Tweets",
+    description: "A Next js Tweets application",
+};
+
+export default function RootLayout({ children}: { children: React.ReactNode }) {
+    return (
+      <ClerkProvider >
+        <html lang='en'>
+          <body className={`${inter.className} bg-dark-1`}>
+            {children}
+          </body>
+        </html>
+      </ClerkProvider>
+    );
+  }
