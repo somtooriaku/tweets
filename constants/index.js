e8bafcb8da3267ecff98464bfb6aa09c6878da36
FxT <somtooriaku@gmail.com>
2023-08-15 05:33:32 -0400

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,45 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/assets/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/assets/search.svg",
+      route: "/search",
+      label: "Search",
+    },
+    {
+      imgURL: "/assets/heart.svg",
+      route: "/activity",
+      label: "Activity",
+    },
+    {
+      imgURL: "/assets/create.svg",
+      route: "/create-tweet",
+      label: "Create tweet",
+    },
+    {
+      imgURL: "/assets/community.svg",
+      route: "/communities",
+      label: "Communities",
+    },
+    {
+      imgURL: "/assets/user.svg",
+      route: "/profile",
+      label: "Profile",
+    },
+  ];
+  
+  export const profileTabs = [
+    { value: "tweets", label: "tweets", icon: "/assets/reply.svg" },
+    { value: "replies", label: "Replies", icon: "/assets/members.svg" },
+    { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" },
+  ];
+  
+  export const communityTabs = [
+    { value: "tweets", label: "tweets", icon: "/assets/reply.svg" },
+    { value: "members", label: "Members", icon: "/assets/members.svg" },
+    { value: "requests", label: "Requests", icon: "/assets/request.svg" },
+  ];
+  
---
+++ lib/models/tweet.model.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const tweetSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  community: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Community",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  parentId: {
+    type: String,
+  },
+  children: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "tweet",
+    },
+  ],
+});
+
+const tweet = mongoose.models.tweet || mongoose.model("tweet", tweetSchema);
+
+export default tweet;
